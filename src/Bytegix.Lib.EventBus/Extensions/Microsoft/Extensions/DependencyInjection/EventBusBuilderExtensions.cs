bb94ebe41490ac4ec68827dd0875030eb5408c86
Michal Potfaj <michal.potfaj@bytegix.com>
2025-08-05 22:23:58 +0200

    Refactoring

--- src/Bytegix.Lib.EventBus/Extensions/Microsoft/Extensions/DependencyInjection/EventBusBuilderExtensions.cs
+++ src/Bytegix.Lib.EventBus/Extensions/Microsoft/Extensions/DependencyInjection/EventBusBuilderExtensions.cs
@@ -45,6 +45,16 @@ public static class EventBusBuilderExtensions
         return eventBusBuilder;
     }
 
+    public static IEventBusBuilder IgnoreDeadLeterForEvent<T>(this IEventBusBuilder eventBusBuilder) where T : IntegrationEvent
+    {
+        eventBusBuilder.Services.Configure<EventBusSubscriptionInfo>(o =>
+        {
+            o.IgnoreDeadLetterEventTypes[typeof(T).Name] = typeof(T);
+        });
+
+        return eventBusBuilder;
+    }
+
     public static IEventBusBuilder AddDeadLetterSubscription<T, [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TH>(this IEventBusBuilder eventBusBuilder)
         where T : IntegrationEvent
         where TH : class, IIntegrationDeadLetterEventHandler<T>
